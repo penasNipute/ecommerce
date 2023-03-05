a2c97cca856c957edffe36445c41f9e448c6ecad
Penas Nipute <penaswild@gmail.com>
2023-03-05 14:00:06 +0200

    desnecessario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export function App() {
+  
 
   return (
-    <div className="App flex bg-purple-3000">
+    <>
       <h1>Hello world</h1>
-    </div>
+    </>
   )
 }
 
-export default App
+
