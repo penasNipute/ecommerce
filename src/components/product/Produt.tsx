447e02f8c606cb184ab1226a569813068e6d7be6
Penas Nipute <penaswild@gmail.com>
2023-03-05 14:47:31 +0200

    git add Product

--- src/components/product/Produt.tsx
+++ src/components/product/Produt.tsx
@@ -1,11 +1,16 @@
+import React from 'react'
 
+
+const desk = 
 
 export function Product(){
 
 
   return(
     <div className="product-card">
-      
+      <div>
+
+      </div>
     </div>
   )
 }
