feb657740a1a002119446efdf81af2f4cd5ece5a
Penas Nipute <penaswild@gmail.com>
2023-03-05 12:08:31 +0200

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App flex bg-purple-3000..........">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
