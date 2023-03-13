3b0fd3745fe2553fa5b10087c834355c275652b8
Joel Junior Manuel Fombe <joeljuniormanuel@gmail.com>
2023-03-13 14:28:53 +0200

    novo

---
+++ src/components/homePage/home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Hero } from "../hero"
+import { NavBar } from "../navbar"
+import { Produtos } from "./produtos"
+
+export const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+            <Hero/>
+            <Produtos/>
+        </div>
+    )
+}
