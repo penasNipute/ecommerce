84c7c61c10a67897257159338db83028376187c6
Penas Nipute <penaswild@gmail.com>
2023-03-31 21:32:45 +0200

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Ads } from './components/Ads/Ads'
 import { ProfileAccount } from './components/ProfileAccount/ProfileAccount' 
-
+import { HomePage } from './components/homePage/home'
 export function App() {
   
 
   return (
     <>
-      <ProfileAccount />
-      <Ads />
+      <HomePage />
     </>
   )
 }
